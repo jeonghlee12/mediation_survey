75a91c68a474e1069ba33e031e880944effdd695
gcamilo <gcamilo7@gmail.com>
2020-08-25 10:17:47 +0900

    config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
-    apiKey: "AIzaSyDX2ksPcjEllCcnIWW3v0iwtqtswQLHxmw",
-    authDomain: "airights-worlds.firebaseapp.com",
-    databaseURL: "https://airights-worlds.firebaseio.com",
-    projectId: "airights-worlds",
-    storageBucket: "airights-worlds.appspot.com",
-    messagingSenderId: "44477616472",
-    appId: "1:44477616472:web:2e02dafb57bd5d6009152d"
-  };
+  apiKey: "AIzaSyBR1kDRHuVAvXe5Ww-QZekReDotNqSr-OA",
+  authDomain: "pg-mediation.firebaseapp.com",
+  databaseURL: "https://pg-mediation.firebaseio.com",
+  projectId: "pg-mediation",
+  storageBucket: "pg-mediation.appspot.com",
+  messagingSenderId: "503836429400",
+  appId: "1:503836429400:web:0c3ed9a01b7705b8d54e2d"
+};
 
-  export default config;
+export default config;
